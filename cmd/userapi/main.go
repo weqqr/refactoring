a2acfaa13dd4f939860dcef5b4b72c9de9089748
weqqr <weqqr@yandex.ru>
2022-11-13 17:43:58 +0300

    Uncomment Recoverer middleware

--- cmd/userapi/main.go
+++ cmd/userapi/main.go
@@ -39,7 +39,7 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
-	//r.Use(middleware.Recoverer)
+	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
